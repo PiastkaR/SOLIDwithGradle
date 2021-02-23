0d416819abee4458c458f33f19a69ef5a92acb15
Rafal <piastka.rafal@gmail.com>
2021-02-23 21:42:44 +0100

    springBoot added

--- src/main/java/com/solid/SolidApp.java
+++ src/main/java/com/solid/SolidApp.java
@@ -1,4 +1,11 @@
 package com.solid;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SolidApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SolidApp.class, args);
+    }
 }
